0b424a19d6de3e944c3b1e60f40199064beef833
hostperson <recepaslan443344@gmail.com>
2017-07-11 14:24:09 +0300

    .java
    helloworld!

---
+++ merhabadunya.java
@@ -0,0 +1,13 @@
+package ilkdeneme1;
+
+public class merhabadunya {
+ public static void main (String[] args)
+ {
+    int sayi_1, sayi_2, sonuc ;
+    sayi_1 = 15;
+    sayi_2 = 20;
+    sonuc = sayi_1*sayi_2;
+    System.out.println("islemin sonucu =" + sonuc );
+    
+ }
+}
