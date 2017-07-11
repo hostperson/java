53f4462fcc09c939421d102451a48eb5603612c1
hostperson <recepaslan443344@gmail.com>
2017-07-11 14:49:19 +0300

    .java
    hesap makinesi  JOptionPane.showMessageDialog ile

--- calculatorApplet.java
+++ calculatorApplet.java
@@ -6,57 +6,45 @@ public class calculatorApplet
     Scanner sc = new Scanner(System.in);
     public static void main(String[] args)
     {
-        JOptionPane.showMessageDialog(null, "Welcome to the Calculator!");
-        String option = JOptionPane.showInputDialog(null, "Which calculator mode do you want?");
-        if (option.equals("Addition"))
+        JOptionPane.showMessageDialog(null,"Hesap Makinesine Hoş Geldiniz!");
+        String option = JOptionPane.showInputDialog(null, "Hangi hangi işlemi yapmak İstediğinizi Seçiniz.örnek.toplama,çıkarma,çarpma,bölme");
+        
+        if (option.equals("toplama"))
         {
-            Double add1 = Double.parseDouble(JOptionPane.showInputDialog(null, "Okay type the first number(s) of your addition problem."));
-            Double add2 = Double.parseDouble(JOptionPane.showInputDialog(null, "Now type the second number(s) of your addition problem."));
+            Double add1 = Double.parseDouble(JOptionPane.showInputDialog(null, "Girmek İstediğiniz İlk Sayıyı Lütfen Yazınız."));
+            Double add2 = Double.parseDouble(JOptionPane.showInputDialog(null, "Lütfen İkinci Sayıyı  Yazınız."));
             Double preAdd = add1+add2;
             Double Add = preAdd;
             JOptionPane.showMessageDialog(null, "The sum is " + Add + ".");
         }
-        else
+  
+        if (option.equals("çıkarma"))
         {
-            JOptionPane.showMessageDialog(null, "Huh?");
-        }
-        if (option.equals("Subtraction"))
-        {
-            Double sub1 = Double.parseDouble(JOptionPane.showInputDialog(null, "Okay type the first number(s) of your subtraction problem."));
-            Double sub2 = Double.parseDouble(JOptionPane.showInputDialog(null, "Now type the second number(s) of your subtraction problem."));
+            Double sub1 = Double.parseDouble(JOptionPane.showInputDialog(null, "Girmek İstediğiniz İlk Sayıyı Lütfen Yazınız."));
+            Double sub2 = Double.parseDouble(JOptionPane.showInputDialog(null, "Lütfen İkinci Sayıyı  Yazınız."));
             Double preSub = sub1-sub2;
             Double Sub = preSub;
-            JOptionPane.showMessageDialog(null, "The difference is " + Sub + ".");
+            JOptionPane.showMessageDialog(null, "fark şu şekil dedir : " + Sub + ".");
         }
-        else
+
+        if (option.equals("çarpma"))
         {
-            JOptionPane.showMessageDialog(null, "Huh?");
-        }
-        if (option.equals("Multiplication"))
-        {
-            Double mult1 = Double.parseDouble(JOptionPane.showInputDialog(null, "Okay type the first number(s) of your multiplication problem."));
-            Double mult2 = Double.parseDouble(JOptionPane.showInputDialog(null, "Now type the second number(s) of your multiplication problem."));
+            Double mult1 = Double.parseDouble(JOptionPane.showInputDialog(null, "Lütfen Çarpma İşlemi İçin İlk Sayıyı Giriniz."));
+            Double mult2 = Double.parseDouble(JOptionPane.showInputDialog(null, "Lütfen Çarpma İşlemi İçin İkinci Sayıyı Giriniz."));
             Double preMult = mult1*mult2;
             Double Mult = preMult;
             JOptionPane.showMessageDialog(null, "The product is " + Mult + ".");
         }
-        else //Here is the error.
+        else 
+        if (option.equals("bölme"))
         {
-            JOptionPane.showMessageDialog(null, "Huh?");
-        } //Here ends the error.
-        if (option.equals("Division"))
-        {
-            Double div1 = Double.parseDouble(JOptionPane.showInputDialog(null, "Okay type the first number(s) of your division problem."));
-            Double div2 = Double.parseDouble(JOptionPane.showInputDialog(null, "Now type the second number(s) of your division problem."));
+            Double div1 = Double.parseDouble(JOptionPane.showInputDialog(null, "Lütfen bölme İşlemi İçin İlk Sayıyı Giriniz."));
+            Double div2 = Double.parseDouble(JOptionPane.showInputDialog(null, "Lütfen bölme İşlemi İçin İkinci Sayıyı Giriniz."));
             Double preDiv = div1/div2;
             Double Div = preDiv;
-            JOptionPane.showMessageDialog(null, "The quotient is " + Div + ".");
+            JOptionPane.showMessageDialog(null, "bölüm sonucu :  " + Div + ".");
         }
-        else
-        {
-            JOptionPane.showMessageDialog(null, "Huh?");
-        }
-        //End of if statements.
+
     } 
 
 }
