a8cf60770f3dc8ad05911721a3c9e7cf6bad42b2
hostperson <recepaslan443344@gmail.com>
2017-07-11 14:26:40 +0300

    .java
    string_example

---
+++ StringBooleanChar.java
@@ -0,0 +1,13 @@
+package ilkdeneme1;
+
+public class StringBooleanChar {
+	public static void main (String[] args)
+    {
+    String nick, site, domain;
+    nick = "LORDHARD";
+    site = "metineller";
+   domain = nick.substring(0,4) +" "+ site.substring(0,5);
+   System.out.println("karýþýk yazým = " + domain);
+// gözünü sevem imla kurallarýna dikkat et
+    	}
+    }
---
+++ stringboolschardevam.java
@@ -0,0 +1,20 @@
+package ilkdeneme1;
+
+public class stringboolschardevam {
+	public static void main (String[] args)
+    {
+    String nick, site, domain;
+    nick = "AAAAB";
+    site = "AAAAD";
+   domain = nick.substring(0,4) +" "+ site.substring(0,5);
+   System.out.println("STRÝNG DEÐÝÞKENLERÝ = " + site.compareTo(nick));
+// gözünü sevem imla kurallarýna dikkat et
+   String isim , isim1;
+   isim = "recep";
+   isim1 = isim.toLowerCase();
+   System.out.println("2.iþlem string deðiþkeni =" + isim1);
+   
+   
+    	}
+
+}
